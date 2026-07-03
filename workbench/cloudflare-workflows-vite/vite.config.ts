09caa8ed6d88ef49a0d73529ec7328256f3485e0
Sree Narayanan <sreeaadhi07@gmail.com>
2026-07-04 02:33:14 +0400

    Chore: Migrate to vite+ 0.2.2 (#8)
    * Migrate to vite+ 0.2.2
    
    * lint

--- workbench/cloudflare-workflows-vite/vite.config.ts
+++ workbench/cloudflare-workflows-vite/vite.config.ts
@@ -1,5 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import { withCloudflareWorkflows } from "../../packages/sideffect/src/vite";
 
 export default defineConfig({
